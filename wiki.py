94fe31fe3214c38299adec80b044743848b1a794
allanfs1 <allanfdsz@gmail.com>
2019-10-10 13:04:00 -0300

    ChatterBot 1.0

--- wiki.py
+++ wiki.py
@@ -1,16 +1,15 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+import pyttsx3
 
-
+speak = pyttsx3.init('sapi5')
 # Create a new chat bot named Charlie
 # Create a new ChatBot instance
 bot = ChatBot(
     'Allan',
     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
     logic_adapters=[
-        'chatterbot.logic.BestMatch',
-        'chatterbot.logic.MathematicalEvaluation'
-
+        'chatterbot.logic.BestMatch'
     ],
     database_uri='mongodb://localhost:27017/chat'
 )
@@ -32,7 +31,9 @@ trainer.train([
     "Eu gostaria de saber com voce estar hoje?",
     "Queria te conhecer melhor",
     "na intimidade?",
-    "como você é?",
+    "E mesmo",
+    "Vamos para outro lugar",
+    "como você estar se vestindo Hoje?",
     "Quantos ano você tem",
     "voçê e legal",
     "Gostei de você",
@@ -40,19 +41,20 @@ trainer.train([
 ])
 
 
-
-
 # Get a response to the input text 'I would like to book a flight.'
-
 # The following loop will execute each time the user enters input
 while True:
     try:
         user_input = input()
-
         bot_response = bot.get_response(user_input)
         bot_response = bot.get_response("conta")
         print(bot_response)
 
+
+        fala = bot_response
+        speak.say(fala)
+        speak.runAndWait()
+
     # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
+    except(KeyboardInterrupt, EOFError, SystemExit):
         break
